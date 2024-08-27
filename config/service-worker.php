caae9d4299b53ff89e6d9b165c35c9de0436d17d
Nils Poltmann <nils@poltmann.com>
2024-08-27 10:43:02 +0200

    Added `service-worker-uri` to config

--- config/service-worker.php
+++ config/service-worker.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
+    'service-worker-uri' => '/build/service-worker.js',
     'service-worker-js-path' => public_path('/build/sw.js'),
 ];
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use poldixd\ServiceWorker\ServiceWorkerController;
 
-Route::get('/build/service-worker.js', ServiceWorkerController::class);
+Route::get(config('service-worker.service-worker-uri'), ServiceWorkerController::class)->name('service-worker-uri');
