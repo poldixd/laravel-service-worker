148414afcd1310f5412c1dae6e1f83772c1fec2c
Nils Poltmann <nils@poltmann.com>
2025-02-04 13:30:51 +0100

    pint

--- src/View/Components/ServiceWorkerJsComponent.php
+++ src/View/Components/ServiceWorkerJsComponent.php
@@ -2,15 +2,15 @@
 
 namespace poldixd\ServiceWorker\View\Components;
 
+use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
-use Closure;
 
 class ServiceWorkerJsComponent extends Component
 {
     public function render(): View|Closure|string
     {
-        return <<<JAVASCRIPT
+        return <<<'JAVASCRIPT'
         <script>
             if('serviceWorker' in navigator) {
                 window.addEventListener('load', () => {
